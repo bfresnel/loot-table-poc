66de75f9855fda50dd51a2671d7bfaf1bea58251
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-30 22:04:44 +0200

    Added test to test startup configuration

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import fr.bfr.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Main.class)
+public class MainTest {
+
+    @Test
+    void loadContext() {
+    }
+}
