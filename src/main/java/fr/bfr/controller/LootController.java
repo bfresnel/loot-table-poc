9ce9f1626e474146ccb9c60a755b9b3a4cc74c8f
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-06-01 00:02:38 +0200

    Updated LootController.java

--- src/main/java/fr/bfr/controller/LootController.java
+++ src/main/java/fr/bfr/controller/LootController.java
@@ -2,7 +2,6 @@ package fr.bfr.controller;
 
 import fr.bfr.api.CharacterRepository;
 import fr.bfr.api.DropRepository;
-import fr.bfr.api.LootApi;
 import fr.bfr.model.Character;
 import fr.bfr.model.DropChance;
 import fr.bfr.services.LootService;
@@ -13,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,18 +20,21 @@ public class LootController {
 
     @Autowired
     private CharacterRepository characterRepository;
-
     @Autowired
     private DropRepository dropRepository;
+    @Autowired
+    private LootService lootService;
+
 
     @GetMapping(value = "/pull", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Character>> pull() throws IOException {
+    public ResponseEntity<List<Character>> pull() {
         List<Character> characters = characterRepository.findAll();
         List<DropChance> dropChances = dropRepository.findAll();
+        List<Character> lootedCharacters = new ArrayList<>();
 
-        // looting system initialization
-        LootApi lootApi = new LootService();
-        List<Character> lootedCharacters = lootApi.pull(characters, dropChances, 10);
+        if (characters.size() > 0 && dropChances.size() > 0) {
+            lootedCharacters = lootService.pull(characters, dropChances, 10);
+        }
 
         return new ResponseEntity<>(lootedCharacters, HttpStatus.OK);
     }
