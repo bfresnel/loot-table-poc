5d7126e9a911456999dc5d51d1e1cf8a6ac84753
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-31 22:59:56 +0200

    Code cleaning

--- src/main/java/fr/bfr/api/CharacterRepository.java
+++ src/main/java/fr/bfr/api/CharacterRepository.java
@@ -1,13 +1,13 @@
 package fr.bfr.api;
 
 import fr.bfr.model.Character;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface CharacterRepository extends Repository<Character, Long> {
+public interface CharacterRepository extends JpaRepository<Character, Long> {
 
     List<Character> findAll();
 }
