4923a7e2de7a310a4ab63c6cf307658f4f423283
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-31 22:59:06 +0200

    Added H2 for test purpose

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,16 +21,18 @@ repositories {
 
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
     implementation("ch.qos.logback:logback-core:1.2.11")
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("org.liquibase:liquibase-core")
+    //implementation("org.liquibase:liquibase-core")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testCompileOnly("org.projectlombok:lombok:1.18.24")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("com.h2database:h2:2.1.212")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,12 @@
 import fr.bfr.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = Main.class)
+@Profile("test")
+@ActiveProfiles("test")
 public class MainTest {
 
     @Test
