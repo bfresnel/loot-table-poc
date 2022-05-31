159c723c416a79b3a30e6fe7043cb7d3ab0f4828
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-31 22:59:56 +0200

    Code cleaning

--- src/main/java/fr/bfr/api/DropRepository.java
+++ src/main/java/fr/bfr/api/DropRepository.java
@@ -1,13 +1,15 @@
 package fr.bfr.api;
 
 import fr.bfr.model.DropChance;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface DropRepository extends Repository<DropChance, Integer> {
+public interface DropRepository extends JpaRepository<DropChance, Integer> {
 
+    @NonNull
     List<DropChance> findAll();
 }
