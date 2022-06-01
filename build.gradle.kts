80fdfe3036e5714caa39fc6c54dc30bd2cfe6bff
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-06-01 20:44:54 +0200

    Data are now persisted in DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("ch.qos.logback:logback-core:1.2.11")
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:1.2.11")
-    //implementation("org.liquibase:liquibase-core")
+    implementation("org.liquibase:liquibase-core")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
---
+++ src/main/resources/db/scripts/init-data.sql
@@ -0,0 +1,13 @@
+INSERT into t_character(id, name, rarity)
+VALUES(1, 'Character 1', 1),
+(2, 'Character 2', 2),
+(3, 'Character 3', 3),
+(4, 'Character 4', 4),
+(5, 'Character 5', 5);
+
+INSERT into t_drop(rarity, chance)
+VALUES(1, 50),
+(2, 25),
+(3, 13),
+(4, 7),
+(5, 5);
