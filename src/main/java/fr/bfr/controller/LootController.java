4069825614a4739771a6d276a5aada076cc90bbd
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-06-03 22:07:06 +0200

    Logging in LootController.java

--- src/main/java/fr/bfr/controller/LootController.java
+++ src/main/java/fr/bfr/controller/LootController.java
@@ -5,6 +5,8 @@ import fr.bfr.api.DropRepository;
 import fr.bfr.model.Character;
 import fr.bfr.model.DropChance;
 import fr.bfr.services.LootService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,22 +20,25 @@ import java.util.List;
 @RestController
 public class LootController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LootController.class);
     @Autowired
     private CharacterRepository characterRepository;
     @Autowired
     private DropRepository dropRepository;
     @Autowired
     private LootService lootService;
-
-
+    
     @GetMapping(value = "/pull", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Character>> pull() {
+        logger.info("/pull endpoint was called !");
         List<Character> characters = characterRepository.findAll();
         List<DropChance> dropChances = dropRepository.findAll();
         List<Character> lootedCharacters = new ArrayList<>();
 
         if (characters.size() > 0 && dropChances.size() > 0) {
+            logger.info("Retrieving all looted characters...");
             lootedCharacters = lootService.pull(characters, dropChances, 10);
+            logger.info("Characters looted successfully !");
         }
 
         return new ResponseEntity<>(lootedCharacters, HttpStatus.OK);
