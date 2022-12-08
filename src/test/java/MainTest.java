ea350f5a67c4de4b666b198717ff8c23c11dfa23
Benoit Fresnel <5629642+bfresnel@users.noreply.github.com>
2022-12-08 09:50:59 +0100

    Updated tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,10 @@
-import fr.bfr.fr.bfr.Main;
+import fr.bfr.MainKt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = Main.class)
+@SpringBootTest(classes = MainKt.class)
 @Profile("test")
 @ActiveProfiles("test")
 class MainTest {
