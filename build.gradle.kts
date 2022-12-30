0e181827b9a4bc5c155aeb739496f914ceca81c1
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-12-30 14:16:13 +0100

    changing json changelogs to xml changelogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,18 +36,20 @@ dependencies {
     implementation("ch.qos.logback:logback-core:1.2.11")
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("org.liquibase:liquibase-core")
+    // we have to set the version because of a bug in 4.9.1 version
+    // https://github.com/liquibase/liquibase/issues/3120
+    implementation("org.liquibase:liquibase-core:4.18.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    
+
     testCompileOnly("org.projectlombok:lombok:1.18.24")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("com.h2database:h2:2.1.214")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    
+
     annotationProcessor("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
@@ -71,10 +73,10 @@ tasks.withType<KotlinCompile> {
 tasks.jar {
     manifest {
         attributes(
-                "Main-class" to "fr.bfr.Main",
-                "Class-Path" to configurations.runtimeClasspath.get()
-                        .filter { it.isFile }
-                        .joinToString(" ") { it.name }
+            "Main-class" to "fr.bfr.Main",
+            "Class-Path" to configurations.runtimeClasspath.get()
+                .filter { it.isFile }
+                .joinToString(" ") { it.name }
         )
     }
 }
@@ -82,11 +84,11 @@ tasks.jar {
 sonarqube {
     properties {
         properties(
-                mapOf<String, Any>(
-                        "sonar.projectKey" to "bfresnel_loot-table-poc",
-                        "sonar.organization" to "bfresnel",
-                        "sonar.host.url" to "https://sonarcloud.io",
-                )
+            mapOf<String, Any>(
+                "sonar.projectKey" to "bfresnel_loot-table-poc",
+                "sonar.organization" to "bfresnel",
+                "sonar.host.url" to "https://sonarcloud.io",
+            )
         )
     }
 }
