d494d3460c81a2d15f90dcdf99ba3646ce5edf2a
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-12-30 21:34:51 +0100

    Added tests for LootService class

--- src/test/kotlin/services/LootServiceTest.kt
+++ src/test/kotlin/services/LootServiceTest.kt
@@ -1,12 +1,40 @@
 package services
 
+import fr.bfr.Main
+import fr.bfr.model.Character
+import fr.bfr.model.DropChance
+import fr.bfr.services.LootService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import fr.bfr.model.Character as CharacterBfr
 
-class LootServiceTest {
+@SpringBootTest(classes = [Main::class])
+@ActiveProfiles("test")
+class LootServiceTest(@Autowired val lootService: LootService) {
 
     @Test
     fun `Assert pull method is working`() {
-        Assertions.assertEquals(true, true)
+        //data
+        val data: List<CharacterBfr> = generateCharacterList()
+        //DropChance
+        val dropChance: List<DropChance> = generateDropChanceList()
+        //NumberOfPull
+        val numberOfPull = 1
+        val result: List<CharacterBfr> = lootService.pull(data, dropChance, numberOfPull)
+        Assertions.assertEquals(result.size, 1)
+        Assertions.assertEquals(result[0].id, 1)
+        Assertions.assertEquals(result[0].name, "bfr")
+        Assertions.assertEquals(result[0].rarity, 1)
+    }
+
+    fun generateCharacterList(): List<CharacterBfr> {
+        return listOf(Character(1, "bfr", 1))
+    }
+
+    fun generateDropChanceList(): List<DropChance> {
+        return listOf(DropChance(1, 100.0))
     }
 }
