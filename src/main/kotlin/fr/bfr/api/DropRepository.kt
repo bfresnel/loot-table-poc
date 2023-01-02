36931c7b51f72762b259849234ea4fc2856361a9
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-01-02 16:41:52 +0100

    [LT-2] Changing some annotations

--- src/main/kotlin/fr/bfr/api/DropRepository.kt
+++ src/main/kotlin/fr/bfr/api/DropRepository.kt
@@ -2,11 +2,9 @@ package fr.bfr.api
 
 import fr.bfr.model.DropChance
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.lang.NonNull
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Repository
 
-@Service
+@Repository
 interface DropChanceRepository : JpaRepository<DropChance, Int> {
-    @NonNull
     override fun findAll(): List<DropChance>
 }
