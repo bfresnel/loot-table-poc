7901d6d7243168f18583a91ca0d4d65ece410460
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-01-02 18:18:05 +0100

    [3.0.0] Kotlin version (#46)
    * Added Develop to all pipelines
    
    * Added kotlin jvm plugin
    
    * Added Main.kt
    
    * Fixing warning error
    
    * Migrating Main class to kotlin
    
    * Updated tests
    
    * Implementing spring data jpa for kotlin
    
    * Migrating all repositories to kotlin
    
    * Changed some service and object to kotlin
    
    * Continuing migration
    
    * changing json changelogs to xml changelogs
    
    * removing unused import
    
    * create gradle.properties to avoid metaspace error in github action
    
    * Finishing migrating business code to kotlin
    
    * [LT-1] Removed MainClass test
    
    * Added tests for LootService class
    
    * Added jacoco report for sonarcloud
    
    * Added new test for LootController
    
    * [LT-2] Optimizing the loot controller (#43)
    
    * [LT-3] Cleaning the gradle.build file (#44)
    
    * [LT-4] Update README.md (#45)
    
    * [LT-2] Optimizing the loot controller (#43)
    
    * [LT-3] Cleaning the gradle.build file (#44)
    
    * [LT-4] Update README.md (#45)

--- src/main/kotlin/fr/bfr/api/CharacterRepository.kt
+++ src/main/kotlin/fr/bfr/api/CharacterRepository.kt
@@ -2,9 +2,9 @@ package fr.bfr.api
 
 import fr.bfr.model.Character
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Repository
 
-@Service
+@Repository
 interface CharacterRepository : JpaRepository<Character, Long> {
     override fun findAll(): List<Character>
 }
